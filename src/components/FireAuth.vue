1a6232e84364ee7d281a8e1ac496d60cc363f8e2
Monica Alba <soeur_azmaria@hotmail.com>
2019-02-06 20:10:09 -0600

    botoncitos con vuetify

--- src/components/FireAuth.vue
+++ src/components/FireAuth.vue
@@ -10,11 +10,13 @@
 <!-- <v-icon>fab fa-facebook-f</v-icon> -->
 <!-- https://fontawesome.com/icons/google?style=brands -->
 
-        <v-btn round light id="Google">Ingresar con Google</v-btn>
-        <v-btn round dark id="Facebook">Ingresar con Facebook</v-btn>
-        <v-btn round dark id="Twitter">Ingresar con Twitter</v-btn>
-        <v-btn round dark id="Github">Ingresar con Github</v-btn>
-        <v-btn round dark id="Email">Ingresar con Email</v-btn>
+    
+
+        <v-btn  class="niceButton" round light id="Google" @click="autenticar(0)"><img :src="imgs[0].img" class="img">  <div class="text"> Ingresar con Google </div></v-btn>
+        <v-btn  class="niceButton" round dark id="Facebook" @click="autenticar(1)"><v-icon class="cool">fab fa-facebook-f</v-icon> <div class="text"> Ingresar con Facebook </div></v-btn>
+        <v-btn  class="niceButton" round dark id="Twitter" @click="autenticar(2)"><v-icon class="cool">fab fa-twitter</v-icon> <div class="text"> Ingresar con Twitter </div></v-btn>
+        <v-btn  class="niceButton" round dark id="Github" @click="autenticar(3)"><v-icon class="cool">fab fa-github</v-icon> <div class="text"> Ingresar con Github </div></v-btn>
+        <v-btn  class="niceButton" round dark id="Email" @click="autenticar(4)"><v-icon class="cool">far fa-envelope</v-icon> <div class="text"> Ingresar con Email </div></v-btn>
 
     </div>
 </template>
@@ -98,18 +100,11 @@ export default {
     justify-content: space-between;
     flex-direction: column;
     align-items: center;
+    
   }
 
   .niceButton{
-    height: 40px;
     width: 300px;
-    border-radius: 5px;
-    color: white;
-    box-shadow: 2px 2px grey;
-    cursor: pointer;
-    margin: auto;
-    margin-top: 20px;
-    margin-bottom: 20px;
   }
 
   #Email{
@@ -134,9 +129,22 @@ export default {
   .img{
     height: 25px;
     width: 25px;
-    margin-top: 5px;
     margin-right: 10px;
-    margin-bottom: -2px;
+  }
+  
+  .cool{
+    margin-right:10px;     
+    margin-left: 5px;
+  }
+
+  .fa-google{
+    color: red!important;
+  }
+
+  .text{
+    width: 200px;
+    text-align: left;
+    margin-left: 10px;
   }
 
 </style>
